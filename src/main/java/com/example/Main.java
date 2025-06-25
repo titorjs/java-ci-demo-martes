fb09e881c88b1ef5a57107d9bfc4b5a740d563e3
Tito Jaramillo S. <titoj740@gmail.com>
2025-06-24 21:00:53 -0500

    Agrega ejecución de checkstyle

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,6 +6,8 @@ public class Main {
         try (PrintWriter w = new PrintWriter("output/index.html", "UTF-8")) {
             w.println("<h1>Hello from Java CI/CD!</h1>");
             System.out.println("HTML generado correctamente.");
+            w.println("<h1>Hello from Java CI/CD!</h1>");
+            w.println("<p><a href=\"checkstyle.html\">Ver reporte Checkstyle</a></p>");
         } catch (IOException e) {
             e.printStackTrace();
         }
